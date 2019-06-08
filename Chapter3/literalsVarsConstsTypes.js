655412c18fc3443320e93c89c247e7bfff7fd586
Njeri <njericooper@gmail.com>
2019-06-07 19:07:37 -0500

    update

--- Chapter3/literalsVarsConstsTypes.js
+++ Chapter3/literalsVarsConstsTypes.js
@@ -149,3 +149,28 @@ arr[arr.length - 1];
 
 const arr = [1, 2, 'c, 4, 5'];
 arr[2] = 3;
+
+//Data Type Conversion
+
+//converting to Numbers
+const numStr = "33.3";
+const num = Number(numStr);
+const a = parseInt("16 volts", 10);
+const b = parseInt("3a", 16);
+const c = parseFloat("15.5 kph");
+//date
+const d = new Date();
+const ts = d.valueOf();
+
+//converting to String
+const n = 33.5;
+n;
+const s = n.toString();
+s;
+
+const arr = [1, true, "hello"];
+arr.toString();
+
+const n = 0;
+const b1 = !!n;
+const b2 = Boolean(n);
