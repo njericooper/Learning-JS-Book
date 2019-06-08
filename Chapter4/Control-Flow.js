02d07a68fb9f697742107c8b00d20bd402cb6d8f
Njeri <njericooper@gmail.com>
2019-06-08 09:58:51 -0500

    Chapter 4, Whitespace

--- Chapter4/Control-Flow.js
+++ Chapter4/Control-Flow.js
@@ -5,3 +5,34 @@ while(funds > 0 && funds < 100) {
     //roll dice
     //collect winnings
 }
+
+//Block Statements
+
+{
+    console.log("statement 1");
+    console.log("statement 2");
+}
+
+let funds = 50;
+
+while(funds > 0 && funds < 100) {
+    funds = funds + 2;
+    funds = funds - 1;
+}
+
+while(funds > 0 && funds < 100)
+    funds = funds + 2;
+
+ // Whitespace
+ 
+ while(funds > 0 && funds < 100)
+
+ funds = funds + 2;
+
+ while(funds > 0 && funds < 100) funds = funds + 2;
+
+ while(funds > 0 && funds < 100) { funds = funds + 2; }
+
+ while(funds > 0 && funds < 100)
+    funds = funds + 2;
+    funds = funds - 1;
