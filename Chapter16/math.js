cf0d9d01f56d8ffb38c17604dfd2917cd44e2c2e
Njeri <njericooper@gmail.com>
2019-06-09 13:16:56 -0500

    End Chapter 17, Begin Chapter 18

---
+++ Chapter16/math.js
@@ -0,0 +1,41 @@
+//Formatting Numbers
+//Fixed Decimals
+const x = 19.51;
+x.toFixed(3);
+x.toFixed(2);
+x.toFixed(1);
+x.toFixed(0);
+
+//exponential Notation
+const x = 3000.5;
+x.toExponential(4);
+x.toExponential(3);
+x.toExponential(2);
+x.toExponential(1);
+x.toExponential(0);
+
+//Fixed Precision
+
+let x = 100;
+x.toPrecision(5);
+x.toPrecision(4);
+x.toPrecision(3);
+
+//Different Bases
+
+const x = 12;
+x.toString();
+x.toString(10);
+x.toString(16);
+x.toString(8);
+x.toString(2);
+
+//Constants
+Math.E 
+Math.PI 
+Math.LN2
+Math.LN10
+Math.LOG2E
+Math.LOG10E
+Math.SQRT1_2
+Math.SQRT2
