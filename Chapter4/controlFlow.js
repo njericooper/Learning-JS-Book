2582581304b02efd71b894a97002773841f4be26
Njeri <njericooper@gmail.com>
2019-06-10 17:31:30 -0500

    block statements

---
+++ Chapter4/controlFlow.js
@@ -0,0 +1,23 @@
+//A Control Flow Primer
+/*//While Loops
+let funds = 50;
+while(funds > 1 && funds < 100) {
+    // place bets
+    // roll dice
+    // collect winnings
+
+}
+console.log(funds);*/
+
+/*//Block Statements
+
+let funds = 50;
+
+while(funds > 1 && funds < 100) {
+    funds = funds + 2;
+    funds = funds - 1;
+}
+console.log(funds);
+while(funds > 1 && funds < 100)
+    funds = funds + 2;
+console.log(funds);*/   
