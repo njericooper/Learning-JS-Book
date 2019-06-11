17976110098b3beb204503813dc1633c5776731d
Njeri <njericooper@gmail.com>
2019-06-10 21:55:32 -0500

    update

--- Chapter2/es6/test.js
+++ Chapter2/es6/test.js
@@ -2,12 +2,12 @@
 const sentences = [
     { subject: 'JavaScript', verb: 'is', object: 'great' },
     { subject: 'elephants', verb: 'are', object: 'large' },
-];
+]
 
-function say({ subject, verb, object }) {
-    console.log(`${subject} ${verb} ${object}`);
+function say ({ subject, verb, object }) {
+    console.log(`${subject} ${verb} ${object}`)
 }
 
-for(let s of sentences){
-    say(s);
+for (let s of sentences) {
+    say(s)
 }
