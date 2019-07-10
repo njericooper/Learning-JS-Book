29c33459dc4c67f0cdf1d5de5179f78177e357bf
Njeri <njericooper@gmail.com>
2019-07-10 08:30:22 -0500

    end Chapter 14

--- Chapter14/asynchronous.js
+++ Chapter14/asynchronous.js
@@ -332,6 +332,66 @@ function ptimeout(delay) {
     });
 }
 
-   
+function grun(g) {
+    const it = g();
+    (function iterate(val) {
+        const x = it.next(val);
+        if(!x.done) {
+            if(x.value instanceof Promise) {
+                x.value.then(iterate).catch(err => it.throw(err));
+            } else {
+                setTimeout(iterate, 0, x.value);
+            }
+        }
+    })();
+}   
+// psudocode, won't run... unless I make datafiles and a filesysyem. Idk about that.
+/*function* theFutureIsNow() {
+    const dataA = yield nfcall(fs.readFile, 'a.txt');
+    const dataB = yield nfcall(fs.readFile, 'b.txt');
+    const dataC = yield nfcall(fs.readFile, 'c.txt');
+    yield ptimeout(60*1000);
+    yield nfcall(fs.writeFile, 'd.txt', dataA+dataB+dataC);
+}
+
+grun(theFutureIsNow);*/
+
+
+//run the files paralell 
+
+function* theFutureIsNow() {
+    const data = yield Promise.all([
+        nfcall(fs.readFile, 'a.txt'),
+        nfcall(fs.readFile, 'b.txt'), 
+        nfcall(fs.readFile, 'c.txt'),
+    ]);
+    yield ptimeout(60*1000);
+    yield nfcall(fs.writeFile, 'd.txt', data[0]+data[1]+data[2]);
+}
+grun(theFutureIsNow);
+
+
+//Dont write my own generator runner. Use the co gen runner
+
+function* theFutureIsNow() {
+    let data;
+    try {
+        data = yield Promise.all([
+            nfcall(fs.readFile, 'a.txt'),
+            nfcall(fs.readFile, 'b.txt'),
+            nfcall(fs.readFile, 'c.txt'),
+        ]);
+    } catch(err) {
+        console.error("unable to read one or more input files: " + err.message);
+        throw err;
+    }
+    yield ptimeout(60*1000);
+    try {
+        yield nfcall(fs.writeFile, 'd.txt', data[0]+data[1]+data[2]);
+    } catch(err) {
+        console.error("Unable to write output file: " + err.message);
+        throw err;
+    }
+}
 
 
