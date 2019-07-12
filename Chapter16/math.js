f48620cba9cdd6f8e3b90fa71a00fa4630d42bf4
Njeri <njericooper@gmail.com>
2019-07-12 09:30:18 -0500

    finished Chapter 16

---
+++ Chapter16/math.js
@@ -0,0 +1,146 @@
+//formatting numbers
+//Fixed Decimals
+
+/*const x = 19.51;
+console.log(x.toFixed(3));
+console.log(x.toFixed(2));
+console.log(x.toFixed(1));
+console.log(x.toFixed(0));
+
+//exponent notation
+const y = 38000.5;
+console.log(y.toExponential(4));
+console.log(y.toExponential(3));
+console.log(y.toExponential(2));
+console.log(y.toExponential(1));
+console.log(y.toExponential(0));
+
+//fixed Precision
+let z = 1000;
+console.log(z.toPrecision(5));
+console.log(z.toPrecision(4));
+console.log(z.toPrecision(3));
+console.log(z.toPrecision(2));
+console.log(z.toPrecision(1));
+z = 15.335;
+console.log(z.toPrecision(6));
+console.log(z.toPrecision(5));
+console.log(z.toPrecision(4));
+console.log(z.toPrecision(3));
+console.log(z.toPrecision(2));
+console.log(z.toPrecision(1));
+
+//Different Bases
+const w = 12;
+console.log(w.toString());
+console.log(w.toString(10));
+console.log(w.toString(16));
+console.log(w.toString(8));
+console.log(w.toString(2));*/
+
+//Constants
+/*console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.LN2);
+console.log(Math.LN10);
+console.log(Math.LOG2E);
+console.log(Math.LOG10E);
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);*/
+
+//Algebraic Functions
+//Exponentiation
+/*console.log(Math.pow(2,3));
+console.log(Math.pow(1.7, 2.3));
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(15.5));
+console.log(Math.cbrt(27));
+console.log(Math.cbrt(22));
+console.log(Math.exp(1));
+console.log(Math.exp(5.5));
+console.log(Math.expm1(1));
+console.log(Math.expm1(5.5)); //e^1-1
+console.log(Math.hypot(3, 4));
+console.log(Math.hypot(2, 3, 4));*/
+
+//Logarithmic Functions
+/*console.log(Math.log(Math.E));
+console.log(Math.log(17.5));
+console.log(Math.log10(10));
+console.log(Math.log10(16.7));
+console.log(Math.log2(2)); //log2 doesn't run
+console.log(Math.log2(5));
+console.log(Math.log1p(Math.E - 1));
+console.log(Math.log1p(17.5));*/ 
+
+//Miscellaneous
+/*console.log(Math.abs(-5.5));
+console.log(Math.abs(5.5));
+console.log(Math.sign(-10.5));
+console.log(Math.sign(6.77));
+console.log(Math.ceil(2.2));
+console.log(Math.ceil(-3.8));
+console.log(Math.floor(2.8));
+console.log(Math.floor(-3.2));
+console.log(Math.trunc(7.7));
+console.log(Math.trunc(-5.8));
+console.log(Math.round(7.2));
+console.log(Math.round(-5.8));
+console.log(Math.round(-7.7));
+console.log(Math.round(-7.2));
+console.log(Math.min(1, 2));
+console.log(Math.min(3, 0.5, 0.66));
+console.log(Math.min(3, 0.5, -0.66));
+console.log(Math.max(1, 2));
+console.log(Math.max(3, 0.5, 0.66));
+console.log(Math.max(-3, 0.5, -0.66));*/
+
+//Pseudorandom Number Generation
+/*console.log(Math.random());
+console.log(3 + (5-3)*Math.random());
+console.log(1 + Math.floor((2-1)*Math.random()));
+console.log(1 + Math.floor((2-1+1)*Math.random()));*/
+
+//Trigonometric Functions
+/*console.log(Math.sin(Math.PI/2));
+console.log(Math.sin(Math.PI/4));
+console.log(Math.cos(Math.PI));
+console.log(Math.cos(Math.PI/4));
+console.log(Math.tan(Math.PI/4));
+console.log(Math.tan(0));
+console.log(Math.asin(0));
+console.log(Math.asin(Math.SQRT1_2));
+console.log(Math.acos(0)); 
+console.log(Math.acos(Math.SQRT1_2));
+console.log(Math.atan(0));
+console.log(Math.atan(Math.SQRT1_2));
+console.log(Math.atan(0, 1));
+console.log(Math.atan(1, 3));
+
+function deg2rad(d) { return d/180*Math.PI; }
+function rad2deg(r) { return r/Math.PI*180; }
+
+console.log(deg2rad(5));
+console.log(rad2deg(4));*/
+
+//Hyperbolic Functions
+console.log(Math.sinh(0));
+console.log(Math.sinh(1));
+console.log(Math.cosh(0));
+console.log(Math.cosh(1));
+console.log(Math.tanh(0));
+console.log(Math.tanh(1));
+console.log(Math.asinh(0));
+console.log(Math.asinh(1));
+console.log(Math.acosh(0));
+console.log(Math.acosh(1));
+console.log(Math.atanh(0));
+console.log(Math.atanh(1));
+
+
+
+
+
+
+
+
