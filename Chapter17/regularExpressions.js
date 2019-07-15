216c6efa1f977b0a747bc048c4ae34c98f2ffecd
Njeri <njericooper@gmail.com>
2019-07-15 17:52:53 -0500

    Start Capter 17

---
+++ Chapter17/regularExpressions.js
@@ -0,0 +1,16 @@
+//Substring matching and replacing
+
+const input = "As I was going to Saint Ives";
+console.log(input.startsWith("As"));
+console.log(input.endsWith("Ives"));
+console.log(input.startsWith("going"), 9);
+console.log(input.endsWith("going", 14));
+console.log(input.includes("going"));
+console.log(input.includes("going", 10));
+console.log(input.indexOf("going"));
+console.log(input.indexOf("going", 10));
+console.log(input.indexOf("nope"));
+console.log(input.toLocaleUpperCase("as"));
+const output = input.replace("going", "walking");
+console.log(output);
+
